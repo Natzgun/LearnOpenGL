ad0171279e550952bd48f8eca5ca7a2b7de03eb3
Natzgun <itzlilouxx@gmail.com>
2025-08-21 20:35:06 -0500

    Create: Shader class for create and compile vertex and fragment shader

---
+++ include/Shader.h
@@ -0,0 +1,25 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader {
+public:
+  unsigned int ID;
+
+  // Constructor reads and build  the shader
+  Shader(const char* vertexPath, const char* fragmentPath);
+
+  // Para activar el shader
+  void use();
+
+  void setBool(const std::string &name, bool value) const;
+  void setInt(const std::string &name, int value) const;
+  void setFloat(const std::string &name, float value) const;
+};
+
+#endif // !SHADER_H
